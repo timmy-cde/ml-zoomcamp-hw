b8b2eb9449abd966d97a975a1b628a47806b5938
Timothy Manuel <timothys.manuel@gmail.com>
2024-10-26 18:54:24 +0800

    until 5.6

---
+++ 05_deployment/predict-test.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import requests
+
+url = 'http://localhost:9696/predict'
+
+customer_id = 'xyz-123'
+customer = {
+    "gender": "female",
+    "seniorcitizen": 0,
+    "partner": "yes",
+    "dependents": "no",
+    "phoneservice": "no",
+    "multiplelines": "no_phone_service",
+    "internetservice": "dsl",
+    "onlinesecurity": "no",
+    "onlinebackup": "yes",
+    "deviceprotection": "no",
+    "techsupport": "no",
+    "streamingtv": "no",
+    "streamingmovies": "no",
+    "contract": "month-to-month",
+    "paperlessbilling": "yes",
+    "paymentmethod": "electronic_check",
+    "tenure": 1,
+    "monthlycharges": 29.85,
+    "totalcharges": 29.85
+}
+
+response = requests.post(url, json=customer).json()
+print(response)
+
+
+if response['churn'] == True:
+    print(f'sending promo email to {customer_id}')
+else:
+    print(f'not sending promo email to {customer_id}')
